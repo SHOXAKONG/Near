55e63d9b7b749ae196b24509fbd4fdc8774cbb5f
SHOXAKONG <bekmurodovshohruh0224@gmail.com>
2025-06-27 15:10:14 +0500

    Fixed Websocket

--- src/core/asgi.py
+++ src/core/asgi.py
@@ -1,12 +1,14 @@
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from src.api.chat.routing import websocket_urlpatterns
-from src.core.middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.core.settings')
 django.setup()
+
+from src.api.chat.routing import websocket_urlpatterns
+from src.core.middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
