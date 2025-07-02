89c76f267be6b0238219740782b51c0e9806dc5f
Shohruh <155909574+SHOXAKONG@users.noreply.github.com>
2025-07-02 17:07:21 +0500

    Changed Models

--- src/api/category/serializers/category.py
+++ src/api/category/serializers/category.py
@@ -1,12 +1,10 @@
-from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 from src.apps.category.models import Category
 
 
 class CategorySerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    subcategories = serializers.SerializerMethodField()
 
     class Meta:
         model = Category
-        fields = ['id', 'name', 'subcategories']
+        fields = ['id', 'name',]
